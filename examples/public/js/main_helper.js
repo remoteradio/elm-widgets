5138d32ec8d9263182574770f74faf4457cc7328
Virgilio T. Dato, Jr <virgilio_dato@yahoo.com>
2015-11-08 19:11:06 +0800

    initial setup

---
+++ examples/public/js/main_helper.js
@@ -0,0 +1,10 @@
+function main() {
+  var elmDiv = document.getElementById('elm');
+
+  var pressTimer = 0;
+  var monitorNumberPressed = '';
+  var elm = Elm.embed(Elm.SvgGraphWidgets.Main, elmDiv,  {
+    
+  });
+
+}
