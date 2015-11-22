9c0bf1b01c4ffb0d9dbaa72bef10fa9f98a5474d
Virgilio T. Dato, Jr <virgilio_dato@yahoo.com>
2015-11-22 23:04:22 +0800

    cleaned package - readying for distribution

--- Gemfile
+++
@@ -1,6 +0,0 @@
-ruby '2.1.3'
-
-source "https://rubygems.org"
-gem "rack", "1.6.4"
-gem "filewatcher", "0.5.2"
-gem "less", "2.6.0"
--- compile-auto-elm-example.sh
+++
@@ -1,3 +0,0 @@
-echo 'watching elm file'
-cd examples/elm && filewatcher '**/*.elm' 'elm-make main.elm --output ../public/js/main.js && cp Html/widgets.elm ../../src/Html/widgets.elm'
-cd ../..
